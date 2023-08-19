b5851ce98c763eed895ec8ee7a97403f6d5e23f9
Wesley Slimani <gintokinotamashii@gmail.com>
2023-08-19 18:21:01 +0200

    feat: fleshed out the About section description more

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,15 @@
 import React from "react"
 
 export const About = () => {
-   return <section className="h-auto bg-light-grey pt-8 pb-60"  id="about">
-      <h2 className="text-4xl text-blue-grey pb-6 font-bold">À propos de moi</h2>
-      <p className="text-xl text-blue-grey"> J'aime créer des sites et apprendre des nouvelles choses dans le milieu.
+   return <section className="h-auto bg-light-grey pt-4 pb-12"  id="about">
+      <h2 className="text-4xl text-blue-grey pb-2 font-bold">À propos de moi</h2>
+      <div className="border-t-2 w-20 m-auto border-dreamy-purple"></div>
+      <p className="text-xl text-blue-grey pt-6"> J'aime créer des sites et apprendre des nouvelles choses dans le milieu, c'est toujours spécial de voir un projet marcher de la manière attendu.
+      <br></br>
+      Suite à mon BTS Systèmes Numériques qui m'a appris les bases pur d'un site web (HTML, CSS, JavaScript).
+      <br></br>
+      Intrigué et curieux, j'ai voulu m'orienter vers le développement web et
+      j'ai donc commencé par apprendre en autoditacte grâce a plusieurs ressources dans le web.
       </p>
    </section>
 }
