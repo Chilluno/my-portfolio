80dda8c21b5ac1e15b5b82b100c53c9f7a815801
Wesley Slimani <gintokinotamashii@gmail.com>
2023-08-20 15:00:37 +0200

    fix: tweaked Routes for deployed page

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
     <Routes>
-     <Route path="/my-portfolio/" element={<Main />}/>
-     <Route path="/my-portfolio/*" element={<Error />} />
+     <Route path="/" element={<Main />}/>
+     <Route path="/*" element={<Error />} />
     </Routes>
     </div>
   );
