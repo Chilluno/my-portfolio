ca8cfbe76d7b27e798081009a08a911be9512be8
Wesley Slimani <gintokinotamashii@gmail.com>
2023-08-20 14:07:51 +0200

    feat: added Skills section to main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,6 +4,8 @@ import { Header } from "../components/Header"
 import { About } from "../components/About"
 import { Projects } from "../components/Projects"
 import { Hero } from "../components/Hero"
+import { Skills } from "../components/Skills"
+
 
 export const Main = () => {
    return <>
@@ -11,6 +13,7 @@ export const Main = () => {
    <Hero />
    <About />
    <Projects />
+   <Skills />
    <Footer />
    </>
 }
