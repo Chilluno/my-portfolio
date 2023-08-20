db087d07fdec43d818f45754080aa1357206b689
Wesley Slimani <gintokinotamashii@gmail.com>
2023-08-20 14:00:02 +0200

    feat: created the Project section and the Project cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,7 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
+import { FaGithub } from "react-icons/fa"
+import { Tag } from "./Tag"
 
-export const ProjectCard = () => {
-   return <div>
-      
+export const ProjectCard = ({title, description, image, repo, date, tags}) => {
+   const [hovered, setHovered] = useState(false);
+
+   const toggleHover = () => {
+      setHovered(!hovered);
+   }
+
+   return <div className="relative rounded-lg shadow-lg overflow-hidden max-w-4xl max-h-2xl" onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
+      <img className="inset-0 h-full w-full rounded-lg object-scale-down"  src={image[0]} alt={title + "'s Homepage"}/>
+      <div className={!hovered ? "opacity-0" : "transition-opacity absolute inset-0 rounded-lg w-full h-auto bg-gradient-to-t from-blue-grey z-10"}>
+      <h3 className="absolute left-4 bottom-[60%] md:top-[50%] text-white text-2xl md:text-3xl">{title}</h3> 
+      <h4 className="absolute left-4 bottom-[48%] md:top-[60%] text-white text-lg md:text-2xl">{date}</h4>
+      <p className="absolute left-4 bottom-[25%]  text-left text-gray-100 text-[3vw] md:text-xl">{description}</p>
+      <div className="absolute left-4 bottom-[5%] md:bottom-[10%] text-sm md:text-base flex gap-3">
+         {
+            tags.map((tag, index) => (
+               <Tag key={index} tag={tag}/>
+            ))
+         }
+      </div>
+      <a href={repo} className="absolute right-4 bottom-[4%] md:bottom-[10%] flex gap-1 text-sm md:text-base text-white text-center rounded-lg bg-black p-1 md:px-2 md:py-2"><FaGithub className="self-center"/> Github</a>
+      </div>
    </div>
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,19 @@
 import React from "react"
+import projects from "../data/projects.json";
+import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
-   return <section className="h-auto font-bold" id="projects">
-   <h2 className="text-4xl text-blue-grey">Projets</h2>   
+
+   return <section className="h-auto font-bold mb-16" id="projects">
+   <h2 className="text-4xl text-blue-grey p-4">Projets</h2>  
+   <div className="border-t-2 w-20 m-auto border-dreamy-purple"></div>
+   <div className="flex flex-col items-center gap-8 px-4 md:px-2">
+      {
+         projects.map((project, index) => (
+            <ProjectCard key={index} title={project.title} image={project.images} tags={project.tags} 
+            description={project.description} repo={project.repository} date={project.date}/>
+         ))
+      }
+   </div> 
    </section>
 }
