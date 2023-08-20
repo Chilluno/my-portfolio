968b165a9993e90815a2df7697d755d0d4a443f9
Wesley Slimani <gintokinotamashii@gmail.com>
2023-08-20 14:06:31 +0200

    fix: tweaked the Hero section's text and made it more responsive

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,12 @@
 import React from "react"
 
 export const Hero = () => {
-   return <section className="min-h-full bg-flex justify-center m-60">
-
-      <div className="text-left">
+   return <section className="min-h-full flex w-auto justify-center m-20 md:m-60">
+      <div className="lg:text-left">
       <h2 className="text-7xl text-blue-grey font-bold mb-2">Wesley Slimani</h2>
       <h3 className="text-3xl font-bold text-blue-grey mb-2">Développeur Front-End</h3>
-      <p className="text-xl">Passionné du développement front-end depuis que j'ai eu des cours basique en HTML,<br></br> CSS et JavaScript à l'école.</p>
+      <p className="text-xl">Passionné du <strong>développement front-end</strong> depuis que j'ai eu des cours basique en HTML,<br></br> CSS et JavaScript.
+      Autoditacte depuis à peu près 1 an.</p>
       </div>
    </section>
 }
