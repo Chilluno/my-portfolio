00e4d86a3dc70d8c2c35d4cf1a31aa687619e112
Wesley Slimani <gintokinotamashii@gmail.com>
2023-08-20 13:58:24 +0200

    fix: tweaked About sections' text

--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,11 @@ export const About = () => {
    return <section className="h-auto bg-light-grey pt-4 pb-12"  id="about">
       <h2 className="text-4xl text-blue-grey pb-2 font-bold">À propos de moi</h2>
       <div className="border-t-2 w-20 m-auto border-dreamy-purple"></div>
-      <p className="text-xl text-blue-grey pt-6"> J'aime créer des sites et apprendre des nouvelles choses dans le milieu, c'est toujours spécial de voir un projet marcher de la manière attendu.
+      <p className="text-xl text-blue-grey pt-6 px-2 md:px-0"> J'aime créer des sites et apprendre des nouvelles choses dans le milieu, c'est toujours spécial de voir un projet marcher de la manière attendu.
       <br></br>
-      Suite à mon BTS Systèmes Numériques qui m'a appris les bases pur d'un site web (HTML, CSS, JavaScript).
+      Après avoir eu mon BTS Systèmes Numériques qui m'a appris les bases pur d'un site web (HTML, CSS, JavaScript).
       <br></br>
-      Intrigué et curieux, j'ai voulu m'orienter vers le développement web et
+      J'ai voulu m'orienter vers le développement web et
       j'ai donc commencé par apprendre en autoditacte grâce a plusieurs ressources dans le web.
       </p>
    </section>
