bff870469ae277de92d6abf4900d9fbc47d9f445
Wesley Slimani <gintokinotamashii@gmail.com>
2023-08-20 13:59:15 +0200

    feat: added links to Footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,28 @@
-import React from "react"
-import {FaLinkedin} from "react-icons/fa"
+import React, { useState } from "react"
+import {FaLinkedin, FaGithub} from "react-icons/fa"
 import { Contact } from "./Contact"
 
 export const Footer = () => {
-   return <div id="contact" className="bg-blue-grey h-auto font-bold">
-   <h2 className="text-4xl text-gray-100">Contact</h2>   
+   const [github, setGithub] = useState(false);
+   const [linkedin, setLinkedin] = useState(false);
+   const toggleGithub = () => setGithub(!github);
+   const toggleLinkedin = () => setLinkedin(!linkedin);
 
-   <div className="m-auto">   
-   <p className="text-gray-100 text-left">wesley-slimani@gmail.com</p>  
-   <FaLinkedin color="white" />
-   </div>
+   return <div id="contact" className="bg-blue-grey h-auto font-bold">
+   <h2 className="text-4xl text-gray-100 p-4">Contact</h2>   
+   <div className="border-t-2 w-20 m-auto border-dreamy-purple"></div>
    <Contact />
-   <p className="text-gray-100">© 2023 Wesley SLIMANI, Tout droits réservés</p>  
+   <div className="flex flex-col items-center">   
+   <a className="text-gray-100 text-left m-2 hover:text-turquoise-blue" href="mailto:wesley.slimani@gmail.com">wesley.slimani@gmail.com</a>
+   <div className="flex flex-row m-2 gap-4"> 
+   <a href="https://www.linkedin.com/in/wesley-slimani-88053b220/">
+   <FaLinkedin color={!linkedin ? "white" : "#00C2CB"} size="40" onMouseEnter={toggleLinkedin} onMouseLeave={toggleLinkedin}/>
+   </a>
+   <a href="https://github.com/Chilluno">
+   <FaGithub color={!github ? "white" : "#00C2CB"} size="40" onMouseEnter={toggleGithub} onMouseLeave={toggleGithub}/>
+   </a>
+   </div> 
+   </div>
+   <p className="text-gray-100 p-6 ">© 2023 Wesley SLIMANI, Tout droits réservés</p>  
 </div>
 }
